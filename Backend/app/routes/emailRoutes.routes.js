d9bb46d5c14a31f25aed85faf43cf34d1718dc16
Fitiavana_R <120584452+FitiavanaRazakarivony@users.noreply.github.com>
2024-09-23 09:48:05 +0300

    updata

--- Backend/app/routes/emailRoutes.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const emailController = require('../controllers/email.controller');
-
-// Route pour envoyer un email
-router.post('/send', emailController.sendEmail);
-
-module.exports = router;
