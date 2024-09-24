07cc1e6b2f8fbf222879e58f92c4e146cbc63118
Fitiavana_R <120584452+FitiavanaRazakarivony@users.noreply.github.com>
2024-09-24 20:49:49 +0300

    update backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,35 +1,43 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const path = require('path'); // N'oubliez pas d'importer 'path'
+const path = require('path');
 
 // Créer l'application Express
 const app = express();
 
-// Middleware
-app.use(cors()); // Pour autoriser les requêtes depuis d'autres domaines (comme Angular)
-app.use(bodyParser.json()); // Pour parser le JSON envoyé dans les requêtes
+// Middleware CORS pour autoriser uniquement une origine spécifique
+const corsOptions = {
+  origin: ['https://fitiavanarazakarivony.vercel.app'], // URL de votre application déployée
+  methods: 'GET,POST,PUT,DELETE',
+  allowedHeaders: 'Content-Type,Authorization'
+};
+app.use(cors(corsOptions));
 
-// Middleware pour servir des fichiers statiques
-app.use(express.static(path.join(__dirname, 'dist/projet'))); // Remplacez par le chemin correct
+// Middleware pour parser le JSON envoyé dans les requêtes
+app.use(bodyParser.json());
+
+// Middleware pour servir des fichiers statiques (exemple Angular)
+app.use(express.static(path.join(__dirname, 'dist/projet')));
 
 // Importer les routes
 const emailRoutes = require('./app/routes/emailRoutes.routes');
 
-// Utiliser les routes
+// Utiliser les routes pour l'API email
 app.use('/api/email', emailRoutes);
 
 // Exemple de route API
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello from Node.js!' });
 });
+
 // Route pour servir l'application Angular
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/projet/browser/index.html')); // Remplacez par le chemin correct
 });
 
 // Démarrer le serveur
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Backend Node.js fonctionne sur le port ${PORT}`);
 });
